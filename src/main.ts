a4d464e3ffd592b337a0cca5e21f06c22cb37904
@ali_abdebaqy <ali.abdelbaqy@grand-community.com>
2023-06-11 20:56:33 +0300

    -w todo

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,5 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+// how angular works ✔️✅
+// basics => 03 video 🔃🙅🏻‍♂️
